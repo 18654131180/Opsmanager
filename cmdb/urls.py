fed7eba1ecb9ac2d91522491e030c68b2939159f
brusezhou <18654131180@163.com>
2019-06-15 13:36:26 +0800

    增加添加公司功能

--- cmdb/urls.py
+++ cmdb/urls.py
@@ -6,10 +6,13 @@ from . import views
 urlpatterns = [
     # 编辑公司
     url(r'edit_company/(?P<company_id>\d+)/$', views.edit_company, name='edit_company'),
+    # 增加公司
+    url(r'new_company', views.new_company, name='new_company'),
      # 主页
     url(r'^$',views.index,name='index'),
     # 公司展示
     url(r'company',views.showcompanys,name='company'),
+
     # 服务器展示
     url(r'server',views.showservers,name='server'),
 
--- cmdb/views.py
+++ cmdb/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.http import Http404,HttpResponseRedirect
+from django.http import Http404, HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from .models import  *
+from .models import *
 from .forms import *
-
 
 
 @login_required
 def index(request):
-    companys=Company.objects.all()
-    context={"companys":companys}
-    return render(request,'cmdb/index.html',context)
+    companys = Company.objects.all()
+    context = {"companys": companys}
+    return render(request, 'cmdb/index.html', context)
+
 
 @login_required
 def showcompanys(request):
@@ -19,14 +19,9 @@ def showcompanys(request):
     context = {"companys": companys}
     return render(request, 'cmdb/index.html', context)
 
-@login_required
-def showservers(request):
-    servers = Servers.objects.all()
-    context = {"servers": servers}
-    return render(request, 'cmdb/server.html', context)
 
 @login_required
-def edit_company(request,company_id):
+def edit_company(request, company_id):
     """实现编辑公司"""
     company = Company.objects.get(id=company_id)
     if request.method != "POST":
@@ -39,3 +34,24 @@ def edit_company(request,company_id):
     context = {"company": company, "form": form}
     return render(request, 'cmdb/edit_company.html', context)
 
+
+@login_required
+def new_company(request):
+    """添加新主题"""
+    if request.method != 'POST':
+        form = CompanyForm
+    else:
+        form = CompanyForm(request.POST)
+        if form.is_valid():
+            # new_company=form.save(commit=False)
+            form.save()
+            return HttpResponseRedirect(reverse('cmdb:company'))  # 通过appname加上urls.py定义的name得到url
+    context = {'form': form}
+    return render(request, 'cmdb/new_company.html', context)
+
+
+@login_required
+def showservers(request):
+    servers = Servers.objects.all()
+    context = {"servers": servers}
+    return render(request, 'cmdb/server.html', context)
