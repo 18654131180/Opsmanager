a4938939f146b8429f748c39a9b1a8ceb8a183f0
brusezhou <18654131180@163.com>
2019-06-15 14:37:36 +0800

    增加增删改服务器功能

--- cmdb/views.py
+++ cmdb/views.py
@@ -8,6 +8,7 @@ from .forms import *
 
 @login_required
 def index(request):
+    '''默认主页，显示公司'''
     companys = Company.objects.all()
     context = {"companys": companys}
     return render(request, 'cmdb/index.html', context)
@@ -15,6 +16,7 @@ def index(request):
 
 @login_required
 def showcompanys(request):
+    '''显示公司'''
     companys = Company.objects.all()
     context = {"companys": companys}
     return render(request, 'cmdb/index.html', context)
@@ -51,7 +53,55 @@ def new_company(request):
 
 
 @login_required
+def del_company(request, company_id):
+    """删除公司"""
+    delcompany = Company.objects.get(id=company_id).delete()
+    if delcompany:
+        return HttpResponseRedirect(reverse('cmdb:company'))
+    context = {"delcopany": delcompany}
+    return render(request, 'cmdb/index.html', context)
+
+@login_required
 def showservers(request):
+    '''显示服务器信息'''
     servers = Servers.objects.all()
     context = {"servers": servers}
+    return render(request, 'cmdb/server.html', context)
+
+@login_required
+def edit_server(request, server_id):
+    """实现编辑公司"""
+    server = Servers.objects.get(id=server_id)
+    if request.method != "POST":
+        form = ServerForm(instance=server)
+    else:
+        form = ServerForm(instance=server, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('cmdb:server'))
+    context = {"server": server, "form": form}
+    return render(request, 'cmdb/edit_server.html', context)
+
+@login_required
+def new_server(request):
+    """添加服务器"""
+    if request.method != 'POST':
+        form = ServerForm
+    else:
+        form = ServerForm(request.POST)
+        if form.is_valid():
+            # new_company=form.save(commit=False)
+            form.save()
+            return HttpResponseRedirect(reverse('cmdb:server'))  # 通过appname加上urls.py定义的name得到url
+    context = {'form': form}
+    return render(request, 'cmdb/new_server.html', context)
+
+
+@login_required
+def del_server(request, server_id):
+    """删除服务器"""
+    delserver = Company.objects.get(id=server_id).delete()
+    if delserver:
+        return HttpResponseRedirect(reverse('cmdb:server'))
+    context = {"delserver": delserver}
     return render(request, 'cmdb/server.html', context)
